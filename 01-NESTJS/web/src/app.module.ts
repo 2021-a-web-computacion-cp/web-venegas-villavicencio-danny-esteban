e00c2e9db298a16b906410554b17966ae9909e1a
nuevo <nuevo>
2021-09-27 12:23:42 -0500

    Proyecto

--- 01-NESTJS/web/src/app.module.ts
+++ 01-NESTJS/web/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    //modulo importados
+    UsuarioModule,
+  ],
+  controllers: [
+    //controladores de este modulo
+    AppController,
+  ],
+  providers: [
+    //servicios de este modulo
+
+    AppService,
+  ],
+  exports: [
+    //servicios exportados (que se pueden usar fuera de éste módulo)
+    AppService,
+  ],
 })
 export class AppModule {}
